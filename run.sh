d029692d1479377fe9279b347ce41d11cd7864e4
Harshini <harshinigirish2001@gmail.com>
2025-09-02 12:43:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env -S bash -l
 set -euo pipefail
 
-# Make conda available in non-interactive shells
+# activate conda
 if [ -f /opt/conda/etc/profile.d/conda.sh ]; then
-  # shellcheck disable=SC1091
   source /opt/conda/etc/profile.d/conda.sh
 else
   echo "conda.sh not found at /opt/conda/etc/profile.d/conda.sh" >&2
   exit 1
 fi
 
-# Activate the env built in the image
+# activate the env created in build.sh
 conda activate subset_watermask_cog
 
-# Avoid leaking user site-packages
-export PYTHONNOUSERSITE=1
-
-# Run the job with the env's python
-exec /opt/conda/envs/subset_watermask_cog/bin/python \
-  /app/OPERA_DPS_JOB/asf-opera-disp-watermask-cog.py "$@"
+# run the main script with any args from DPS
+/opt/conda/envs/subset_watermask_cog/bin/python /app/OPERA_DPS_JOB/asf-opera-disp-watermask-cog.py "$@"
