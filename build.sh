8bbe2dc6297b7fd383dab684df0cac4115f6eb80
Harshini <harshinigirish2001@gmail.com>
2025-09-09 12:34:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ENV_PREFIX="/opt/conda/envs/opera-env"
+ENV_PREFIX="/opt/conda/envs/subset_watermask_cog"
 
 conda env create -f environment.yml --prefix "$ENV_PREFIX"
 conda clean -afy
 
 # Sanity check
 conda run -p "$ENV_PREFIX" python - <<'PY'
-import xarray, rioxarray, rasterio, s3fs, shapely, pyproj, netCDF4, h5netcdf, scipy
+import xarray, rioxarray, rasterio, s3fs, shapely, pyproj, netCDF4, h5netcdf, scipy, requests
 from maap.maap import MAAP
 print("Conda env OK")
 PY
