722dc9641fdc08ced5a5bf63777f1da7a9b1a1d6
Harshini <harshinigirish2001@gmail.com>
2025-09-09 12:53:06 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENV_PREFIX="/opt/conda/envs/subset_watermask_cog"
 
 # point directly at the env file that lives next to this script
-conda env create -f "${basedir}/environment.yml" --prefix "$ENV_PREFIX"
+conda env create -f "${basedir}/env.yml" --prefix "$ENV_PREFIX"
 conda clean -afy
 
 # Sanity check
